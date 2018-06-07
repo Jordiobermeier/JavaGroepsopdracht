eef9593800a5ed46e04e90606c824c9d48fedaa1
Re: Software Intern <30954489+resoftware-intern@users.noreply.github.com>
2018-06-07 13:20:10 +0200

    Button Fixes

--- VanguardApp/src/Button.java
+++ VanguardApp/src/Button.java
@@ -1,4 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Button extends Circle{
+import javax.swing.*;
+
+public class Button extends JPanel {
+
+	static JFrame frame;
+	
+    public Button() {
+    	JButton endTurn = new JButton("Turn End");
+    	EndTurnHandler turnEnd = new EndTurnHandler();
+        JButton reset = new JButton("reset");
+        JButton apply = new JButton("Apply");
+        add(reset);
+        add(endTurn);
+        add(apply);
+        endTurn.addActionListener(turnEnd);
+        setVisible(true);
 
 }
+    class EndTurnHandler implements ActionListener {
+    public void actionPerformed( ActionEvent e) {
+    JDialog d = new JDialog ( frame, "Turn End", true);
+    d.setLocationRelativeTo(frame);
+    d.setVisible(true);
+    }
+    }
+}
+
+
--- VanguardApp/src/Controller.java
+++ VanguardApp/src/Controller.java
@@ -2,12 +2,6 @@ import javax.swing.*;
 
 public class Controller extends JPanel {
 
-    public Controller() {
 
-        JButton button = new JButton("test");
-        add(button);
-        setVisible(true);
-
-}
-    
+ 
 }
