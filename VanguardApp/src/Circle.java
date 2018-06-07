8e4c219573fde6fa61287208810aeffc14b9ccac
DESKTOP-0HFV3SC\Danique Loman <daniqueloman@gmail.com>
2018-06-07 13:27:24 +0200

    qfeqw
    wef

--- VanguardApp/src/Circle.java
+++ VanguardApp/src/Circle.java
@@ -2,29 +2,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
-public class Circle extends VanguardApp {
+public class Circle extends JPanel {
 	JTextField inputBase, inputPower, outputTotal;
-	JButton reset;
 	public Circle() {
 		inputBase = new JTextField( 10 );
 		inputPower = new JTextField( 10 );
 		outputTotal = new JTextField( 10 );
-
-		reset = new JButton();
-		
-		reset.addActionListener( new resetHandler() );
 		
 		add( inputBase );
 		add( inputPower );
 		add( outputTotal );
-		add( reset );
 		
-		}
-	class resetHandler implements ActionListener {
-		public void actionPerformed( ActionEvent e ) {
-}
 }
 }
